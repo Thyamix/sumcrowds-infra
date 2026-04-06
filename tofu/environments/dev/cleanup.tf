4d9a1f748b37f387f54802cff94de26cc22868c4
Thyamix <contact@thyamix.com>
2026-04-07 00:25:19 +0200

    feat: cleanup config

--- tofu/environments/dev/cleanup.tf
+++ tofu/environments/dev/cleanup.tf
@@ -7,6 +7,9 @@ resource "kubernetes_cron_job_v1" "cleanup_cron" {
 	}
 	spec {
 		schedule = "@midnight"
+		concurrency_policy = "Forbid"
+		failed_jobs_history_limit = 1
+		successful_jobs_history_limit = 1
 		job_template {
 			metadata {
 				name = "cleanup-job"
@@ -18,10 +21,17 @@ resource "kubernetes_cron_job_v1" "cleanup_cron" {
 						name = "cleanup"
 					}
 					spec {
+						restart_policy = "OnFailure"
 						container {
 							name = "cleanup"
 							image = "git.thyamix.com/thyamix/sumcrowds-cleanup:latest"
 							image_pull_policy = "Always"
+							resources {
+								limits = {
+									cpu = "500m"
+									memory = "512Mi"
+								}
+							}
 							volume_mount {
 								name = "config-volume"
 								mount_path = "/app/config.dev.toml"
@@ -39,11 +49,6 @@ resource "kubernetes_cron_job_v1" "cleanup_cron" {
 							env {
 								name = "CONFIG_PATH"
 								value = "/app/config.dev.toml"
-							}
-							env_from {
-								secret_ref {
-									name = "backend"
-								}
 							}
 						}
 						volume {
